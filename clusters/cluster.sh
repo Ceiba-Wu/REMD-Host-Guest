cb0424bd432d74650463b91332e3ecda1cd4981d
Leyun Wu <91232561+Ceiba-Wu@users.noreply.github.com>
2025-11-24 21:59:32 +0800

    Update cluster.sh

--- clusters/cluster.sh
+++ clusters/cluster.sh
@@ -1,1 +1,18 @@
-
+cpptraj <<EOF
+parm dry.top
+trajin dry.xtc
+autoimage
+cluster C1 \
+        hieragglo epsilon 2.0 linkage epsilonplot eposilonplot.dat \
+        rms :1-2 \
+        sieve 50 random \
+        out cnumvtime.dat \
+        sil Sil \
+        summary summary.dat \
+        info info.dat \
+        cpopvtime cpopvtime.agr normframe \
+        repout rep repfmt pdb \
+        singlerepout singlerep.nc singlerepfmt netcdf \
+        avgout Avg avgfmt restart
+run
+EOF
